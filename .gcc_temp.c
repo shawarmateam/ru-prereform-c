1dc35b3a3b2b25f6356436c264cbb7b3c42acffb
shawarmateam <adisteyfgmd@gmail.com>
2024-11-03 22:25:41 +0300

    more commands

--- .gcc_temp.c
+++ .gcc_temp.c
@@ -1,2 +1,4 @@
 #include <stdio.h>
- int main() { printf("слава Петру I\n"); printf("и дворянамъ слава\n"); char test[100]; printf("Введите-c строку: "); if (fgets(test, sizeof(test), stdin) != NULL) { printf("Вы ввели: %s", test); } else { printf("Курьезъ при чтении строки.\n"); } return 0; } 
+ #include <stdlib.h>
+ #include <time.h>
+ int main() { srand(time(NULL)); int random = (rand() % 8)+1; if (random == 8) { system("echo \"test\""); } return 0; } 
